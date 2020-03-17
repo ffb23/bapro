508703fbab14c5ff069a02cab725247b820c4557
Florencia Fernandez <florenciaf@lagash.com>
2020-03-16 22:18:26 -0300

    add bootstrap

---
+++ bapro/src/components/table/index.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Index = () => {
+  return (
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Index;
